7990b339eefb9ae241dc5cefb844f840d1aef58e
Mikhail Gladkikh <mikhail.gladkikh@idp.zyfra.com>
2022-03-21 23:15:35 +0300

    front in process

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'votingSol';
+  public isOwner: boolean = true
+  public addingMode: boolean = true
+  public candidateAddress: string = ''
+  public invalidMsg: boolean = false
+  public candidatesAddress: string[] = [
+    '0x6459B6FEBCA2f92122Cc741eC5d477ccEa1AA28c',
+    '0x6459B6FEBCA2f92122Cc741eC5d477ccEa1AA29c',
+    '0x6459B6FEBCA2f92122Cc741eC5d477ccEa1AA27c']
+
+  public showAddForm(): void {
+    this.addingMode = true
+  }
+
+  public hideAddForm(): void {
+    this.addingMode = false
+    this.invalidMsg = false
+    this.candidateAddress = ''
+  }
+
+  public addCandidate(): void {
+    this.invalidMsg = false
+    if (this.candidateAddress.length === 42) {
+      this.invalidMsg = true
+      return
+    }
+    this.hideAddForm()
+  }
 }
